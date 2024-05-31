8e44f58d51e9b4428cfe892f227e378ffb6c9b00
lamarmellitus1 <92574617+lamarmellitus1@users.noreply.github.com>
2024-05-31 14:10:31 +0100

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,9 +2,9 @@
 
 def call(){
     echo 'building a docker image & deploying to dockerhub'
-    withCredentials([string(credentialsId: 'passwds', variable: 'dockerwd')]){
-        sh 'docker build -t mellitus/java-web-app:latest . '
-        sh "docker login -u mellitus -p ${dockerwd}"
-        sh "docker push mellitus/java-web-app:latest"
-    }
+    // withCredentials([string(credentialsId: 'passwds', variable: 'dockerwd')]){
+    //     sh 'docker build -t mellitus/java-web-app:latest . '
+    //     sh "docker login -u mellitus -p ${dockerwd}"
+    //     sh "docker push mellitus/java-web-app:latest"
+    // }
 }
